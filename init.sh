a786a63325579b2ac2650083cbf202de691b69c9
ViktorNech <buktop20@gmail.com>
2016-05-19 16:12:46 +0300

    test 1 complete

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/kicune/stepic/my_stepic_web_project/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
