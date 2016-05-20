06a6c3c0879b2368512f6218628f22159d7c79e3
ViktorNech <buktop20@gmail.com>
2016-05-21 02:49:21 +0300

    run with debug

---
+++ ask/ask/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+from qa.views import test
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    # url(r'^admin/', include(admin.site.urls)),
+    # url(r'^polls/', include('polls.urls')),
+    url(r'^question/', include('qa.urls')),
+    url(r'^$', test),
+    url(r'^login/$', test),
+    url(r'^signup/$', test),
+    url(r'^ask/$', test),
+    url(r'^popular/$', test),
+    url(r'^new/$', test),
+
+)
--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+git config --global user.name "ViktorNech"
+git config --global user.email "buktop20@gmail.com"
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py.conf
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
-#sudo gunicorn -c /etc/gunicorn.d/hello.py.conf
 
