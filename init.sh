d7686a28fb9a7d28e999dd08be6997ff8eb9fee0
ViktorNech <buktop20@gmail.com>
2016-05-20 04:08:40 +0300

    update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo ln -sf /home/kicune/stepic/my_stepic_web_project/etc/nginx.conf /etc/nginx/
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/kicune/stepic/my_stepic_web_project/hello.py.conf /etc/gunicorn.d/hello.py.conf
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c /etc/gunicorn.d/hello.py.conf
+#sudo gunicorn -c /etc/gunicorn.d/hello.py.conf
 
